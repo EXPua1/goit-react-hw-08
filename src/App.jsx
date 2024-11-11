3c4cec2306f54a1cef9053ec10c024d5658be56a
EXPua1 <161503064+EXPua1@users.noreply.github.com>
2024-11-11 20:06:06 +0200

    fixing

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import React, { useState, useEffect, lazy } from "react";
-import ReactDOM from "react-dom/client";
+import React, { useState, useEffect, lazy, Suspense } from "react";
 import "modern-normalize";
 import "./index.css";
 import { Container, Layout, Section } from "./components";
 import { Route, Routes } from "react-router-dom";
-import HomePage from "./pages/HomePage/homePage";
-import RegisterPage from "./pages/RegisterPage/registerPage";
-import ContactsPage from "./pages/ContactsPage/contactsPage";
-import LoginPage from "./pages/LoginPage/loginPage";
 import { RestrictedRoute } from "./RestrictedRoute";
 import { apiRefreshUser } from "./redux/auth/operations";
 import { useDispatch, useSelector } from "react-redux";
 import { selectIsRefreshing } from "./redux/auth/selectors";
 import PrivateRoute from "./PrivateRoute";
+
+const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
+const ContactsPage = lazy(() => import("./pages/ContactsPage/ContactsPage"));
+const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
+const RegisterPage = lazy(() => import("./pages/RegisterPage/RegisterPage"));
 
 const App = () => {
   const isRefreshing = useSelector(selectIsRefreshing);
@@ -23,44 +23,41 @@ const App = () => {
   }, [dispatch]);
 
   if (isRefreshing) {
-    return <p>Loading...</p>; // Можно заменить на компонент загрузки или индикатор
+    return <p>Loading...</p>;
   }
-
-  const HomePage = lazy(() => import("./pages/HomePage/HomePage"));
-  const ContactsPage = lazy(() => import("./pages/ContactsPage/ContactsPage"));
-  const LoginPage = lazy(() => import("./pages/LoginPage/LoginPage"));
-  const RegisterPage = lazy(() => import("./pages/RegisterPage/RegisterPage"));
 
   return (
     <Section>
       <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route
-            path="/register"
-            element={
-              <RestrictedRoute
-                redirectTo="/contacts"
-                component={<RegisterPage />}
-              />
-            }
-          />
-          <Route
-            path="/login"
-            element={
-              <RestrictedRoute
-                redirectTo="/contacts"
-                component={<LoginPage />}
-              />
-            }
-          />
-          <Route
-            path="/contacts"
-            element={
-              <PrivateRoute redirectTo="/login" element={<ContactsPage />} />
-            }
-          />
-        </Routes>
+        <Suspense fallback={<p>Loading page...</p>}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route
+              path="/register"
+              element={
+                <RestrictedRoute
+                  redirectTo="/contacts"
+                  component={<RegisterPage />}
+                />
+              }
+            />
+            <Route
+              path="/login"
+              element={
+                <RestrictedRoute
+                  redirectTo="/contacts"
+                  component={<LoginPage />}
+                />
+              }
+            />
+            <Route
+              path="/contacts"
+              element={
+                <PrivateRoute redirectTo="/login" element={<ContactsPage />} />
+              }
+            />
+          </Routes>
+        </Suspense>
       </Layout>
     </Section>
   );
